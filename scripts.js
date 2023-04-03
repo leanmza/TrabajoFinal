4cfd9ed88e1f1da6f4c660b0bfebd74fd47c609a
MatiasVill <matias_numl@hotmail.com>
2023-04-03 19:59:49 -0300

    Agregado wizard
    Agregado formulario, y reparado nav bar

--- scripts.js
+++ scripts.js
@@ -1,31 +1,150 @@
 let contacto = document.getElementById("contacto"); //div de contacto
 let cuerpo = document.getElementById("cuerpo");
+let formulario = document.getElementById("wizardform");
 window.addEventListener("load",inicio)
 
 let btnContacto = document.getElementById("btn-contacto"); //boton/link del header
 let btnCuerpo = document.getElementById("btn-cuerpo");
 let btnNosotros = document.getElementById("btn-nosotros");
+let btnWizard = document.getElementById("btn-wizard");
+
 btnContacto.addEventListener("click",mostrarContacto);
 btnCuerpo.addEventListener("click",mostrarCuerpo);
 btnNosotros.addEventListener("click",mostrarCuerpo);
+btnWizard.addEventListener("click",mostrarFormulario);
 
 
-let controlContacto = false
-let controlNosotros = true
 
 function inicio(){
  contacto.style.display = "none"
+ formulario.style.display = "none"
 }
 
 function mostrarContacto(){
     contacto.style.display = "grid";
     cuerpo.style.display = "none";
-    controlContacto = true
-    controlNosotros = false
+	formulario.style.display = "none"
 }
  function mostrarCuerpo(){
     contacto.style.display = "none";
     cuerpo.style.display = "block";
-    controlContacto = false
-    controlNosotros = true
- }
+	formulario.style.display = "none"
+}
+function mostrarFormulario(){
+    contacto.style.display = "none";
+    cuerpo.style.display = "none";
+	formulario.style.display = "grid"
+}
+
+function scroll_to_class(element_class, removed_height) {
+	var scroll_to = $(element_class).offset().top - removed_height;
+	if($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({scrollTop: scroll_to}, 0);
+	}
+}
+
+function bar_progress(progress_line_object, direction) {
+	var number_of_steps = progress_line_object.data('number-of-steps');
+	var now_value = progress_line_object.data('now-value');
+	var new_value = 0;
+	if(direction == 'right') {
+		new_value = now_value + ( 100 / number_of_steps );
+	}
+	else if(direction == 'left') {
+		new_value = now_value - ( 100 / number_of_steps );
+	}
+	progress_line_object.attr('style', 'width: ' + new_value + '%;').data('now-value', new_value);
+}
+
+jQuery(document).ready(function() {
+	
+    
+    /*
+        Form
+    */
+    $('.f1 fieldset:first').fadeIn('slow');
+    
+    $('.f1 input[type="text"], .f1 input[type="password"], .f1 textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    // next step
+    $('.f1 .btn-next').on('click', function() {
+    	var parent_fieldset = $(this).parents('fieldset');
+    	var next_step = true;
+    	// navigation steps / progress steps
+    	var current_active_step = $(this).parents('.f1').find('.f1-step.active');
+    	var progress_line = $(this).parents('.f1').find('.f1-progress-line');
+    	
+    	// fields validation
+    	parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function() {
+    		if( $(this).val() == "" ) {
+    			$(this).addClass('input-error');
+    			next_step = false;
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	// fields validation
+    	
+    	if( next_step ) {
+    		parent_fieldset.fadeOut(400, function() {
+    			// change icons
+    			current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+    			// progress bar
+    			bar_progress(progress_line, 'right');
+    			// show next step
+	    		$(this).next().fadeIn();
+	    		// scroll window to beginning of the form
+    			scroll_to_class( $('.f1'), 20 );
+	    	});
+    	}
+    	
+    });
+    
+    // previous step
+    $('.f1 .btn-previous').on('click', function() {
+    	// navigation steps / progress steps
+    	var current_active_step = $(this).parents('.f1').find('.f1-step.active');
+    	var progress_line = $(this).parents('.f1').find('.f1-progress-line');
+    	
+    	$(this).parents('fieldset').fadeOut(400, function() {
+    		// change icons
+    		current_active_step.removeClass('active').prev().removeClass('activated').addClass('active');
+    		// progress bar
+    		bar_progress(progress_line, 'left');
+    		// show previous step
+    		$(this).prev().fadeIn();
+    		// scroll window to beginning of the form
+			scroll_to_class( $('.f1'), 20 );
+    	});
+    });
+    
+    // submit
+    $('.f1').on('submit', function(e) {
+    	
+    	// fields validation
+    	$(this).find('input[type="text"], input[type="password"], textarea').each(function() {
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	// fields validation
+    	
+    });
+    
+    
+});
+
+$(document).ready(function(){
+	
+	$('input[type="text"], input[type="password"], textarea').each(function() {
+		$(this).val( $(this).attr('placeholder') );
+    });
+	
+});
